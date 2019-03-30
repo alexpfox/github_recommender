6a4408d0c6eba1900e2e394c33c2d9ffd3898c9c
Alex Murray <horisustar@gmail.com>
2019-03-30 16:40:00 -0700

    pull latest changes from aws instance

--- flask_app/app.py
+++ flask_app/app.py
@@ -23,38 +23,3 @@ def index():
     return render_template('index.html',
         username=username,
         prediction=lines)
-
-
-"""
-from flask import Flask
-from flask import render_template
-from flask import request
-
-from generate import generate
-
-app = Flask(__name__)
-
-def init():
-    print('Starting...')
-
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('hello.html', user=name)
-
-@app.route('/')
-def index():
-    author = request.args.get('author', None)
-    seed = request.args.get('seed', '')
-
-    # our generateor returns a list of lines for a poem
-    lines = []
-    if author is not None:
-        lines = generate(author, seed)
-
-    return render_template('index.html',
-        author=author,
-        seed=seed,
-        poem=lines)
-
-
-"""
